52cacd6f37cdc6faac8e83e17616c0426dbb7db4
Michael Bilevich <michaelmoshe@mail.tau.ac.il>
2024-12-18 19:46:28 +0200

    Add loading of poly files

--- examples/demo_lab446.py
+++ examples/demo_lab446.py
@@ -6,5 +6,8 @@ import matplotlib.pyplot as plt
 import sdsl
 from sdsl.visualization.viz2d import visualize_2d
 
+MAP_PATH = "resources/maps/lab_lidar.poly"
+
 if __name__ == "__main__":
-    pass
+    env = sdsl.load_poly_file(MAP_PATH)
+    visualize_2d(env)
--- sdsl/src/sdsl/__init__.py
+++ sdsl/src/sdsl/__init__.py
@@ -1,7 +1,35 @@
 from .sdsl import __doc__, __version__
-from .sdsl import R2xS1, R2xS1_Voxel, Env_R2
+from .sdsl import R2xS1, R2xS1_Voxel, Env_R2, localize_R2
 from .sdsl import max_min_on_trig_range
 
+import numpy as np
+
 __all__ = [
-    "R2xS1", "R2xS1_Voxel", "Env_R2"
+    "R2xS1", "R2xS1_Voxel", "Env_R2", "localize_R2",
+    "max_min_on_trig_range",
 ]
+
+def load_poly_file(filename: str) -> Env_R2:
+    polygons = []
+    curr_polygon = []
+    with open(filename, "r") as fp:
+        for line in fp.readlines():
+            line = line.strip()
+            if line == "":
+                polygons.append(curr_polygon)
+                curr_polygon = []
+            else:
+                x, y = line.split()
+                x, y = float(x), float(y)
+                curr_polygon.append([x, y])
+    if len(curr_polygon) > 0:
+        polygons.append(curr_polygon)
+
+    # Convert to numpy array of segments
+    segments = []
+    for polygon in polygons:
+        for i in range(len(polygon)):
+            segments.append([*polygon[i], *polygon[(i+1) % len(polygon)]])
+    segments = np.array(segments, dtype=np.double)
+
+    return Env_R2(segments)
