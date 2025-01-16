48196b6f1a00da9e420f1c2f1443ba3d776aec4a
PC-DANHA4 <cgl@cs.tau.ac.il>
2025-01-16 13:20:23 +0200

    Serialize the raw rosbag trajectory

--- data/data2d/ros_frog/bag_files/serialize_traj.py
+++ data/data2d/ros_frog/bag_files/serialize_traj.py
@@ -1,6 +1,11 @@
+import pickle
+
 import nml_bag
 
 if __name__ == "__main__":
     reader = nml_bag.Reader("traj/traj_0.mcap", topics=["/scan_idx", "/pose"])
+    records = []
     for record in reader:
-        print(record)
+        records.append(record)
+    with open("traj_raw.pkl", "wb") as fp:
+        pickle.dump(records, fp)
