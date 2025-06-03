a3278fff4bffdc80e89383aa0b48d5f14fedd456
Michael Bilevich <michaelmoshe@mail.tau.ac.il>
2025-06-03 17:51:13 +0300

    Remove old data files

--- data/data2d/ros_frog/start.zsh
+++
@@ -1,8 +0,0 @@
-#!/bin/zsh
-
-# MAKE SURE TO ADD THIS ENVIRONMENT VARIABLE
-# export FROG_RAW_DATA="..."
-export RMW_IMPLEMENTATION=rmw_cyclonedds_cpp
-source ./install/setup.zsh
-# ros2 run frog_publisher publisher
-ros2 launch frog_publisher frog.launch.py
--- scripts/common.py
+++
@@ -1,9 +0,0 @@
-import os
-
-
-SEED = 100
-
-RESULTS_DIR = "results"
-RESOURCES_DIR = "resources"
-MAPS_DIR = os.path.join(RESOURCES_DIR, "maps")
-
