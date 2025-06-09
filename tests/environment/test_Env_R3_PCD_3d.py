72eb24c567edd9c9c7fd8976cfe31a82488988b0
Michael Bilevich <michaelmoshe@mail.tau.ac.il>
2025-06-09 12:11:18 +0300

    Add 3d pcd init test

---
+++ tests/environment/test_Env_R3_PCD_3d.py
@@ -0,0 +1,16 @@
+import numpy as np
+
+import sdsl
+
+
+TEST_FILE = "resources/maps/3d/lab446a.ply"
+TOLERANCE = 1e-7
+
+def test_Env_R3_PCD_init_3d():
+    arr = sdsl.loaders.load_pcd_3d(TEST_FILE)
+    env = sdsl.Env_R3_PCD(arr)
+    assert np.allclose(
+        np.sort(env.get_representation(), axis=0),
+        np.sort(arr, axis=0),
+        atol=TOLERANCE
+    )
